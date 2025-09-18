473b1f5bfbe644425c8d55e42d2e0dbdbdcd1eec
CandiceGuo <guoqian14@huawei.com>
2025-09-18 19:58:19 +0800

    refactor: 增加workflow_state

--- jiuwen/core/runtime/state.py
+++ jiuwen/core/runtime/state.py
@@ -67,8 +67,11 @@ IO_STATE_UPDATES_KEY = "io_state_updates"
 GLOBAL_STATE_KEY = "global_state"
 GLOBAL_STATE_UPDATES_KEY = "global_state_updates"
 COMP_STATE_KEY = "comp_state"
+WORKFLOW_STATE_KEY = "workflow_state"
 COMP_STATE_UPDATES_KEY = "comp_state_updates"
+WORKFLOW_STATE_UPDATES_KEY = "workflow_state_updates"
 DEFAULT_NODE_ID = "default"
+DEFAULT_WORKFLOW_ID = "workflow"
 
 
 class State(ABC):
@@ -77,6 +80,7 @@ class State(ABC):
             io_state: CommitStateLike,
             global_state: CommitStateLike,
             comp_state: CommitStateLike,
+            workflow_state: CommitStateLike,
             trace_state: dict = {},
             parent_id: str = '',
             node_id: str = DEFAULT_NODE_ID
@@ -85,6 +89,7 @@ class State(ABC):
         self._global_state = global_state
         self._trace_state = trace_state
         self._comp_state = comp_state
+        self._workflow_state = workflow_state
         self._parent_id = parent_id
         self._node_id = node_id
 
@@ -125,11 +130,21 @@ class CommitState(State):
     def __init__(self, io_state: CommitStateLike,
                  global_state: CommitStateLike,
                  comp_state: CommitStateLike,
+                 workflow_state: CommitStateLike,
                  trace_state: dict = {},
                  parent_id: str = '',
                  node_id: str = DEFAULT_NODE_ID):
         super().__init__(io_state=io_state, global_state=global_state, comp_state=comp_state, trace_state=trace_state,
-                         parent_id=parent_id, node_id=node_id)
+                         workflow_state=workflow_state, parent_id=parent_id, node_id=node_id)
+
+    def get_workflow_state(self, key: Union[str, list, dict]) -> Optional[Any]:
+        if self._workflow_state is None or key is None:
+            return None
+        return self._workflow_state.get(key)
+
+    def update_and_commit_workflow_state(self, data: dict):
+        self._workflow_state.update_by_id(DEFAULT_WORKFLOW_ID, data)
+        self._workflow_state.commit()
 
     def set_outputs(self, data: dict) -> None:
         if self._io_state is None or data is None:
@@ -138,7 +153,7 @@ class CommitState(State):
 
     def get_inputs(self, schema: Union[str, list, dict] = None) -> Optional[Any]:
         if self._io_state is None:
-            return
+            return None
         if schema is None:
             return self._io_state.get(self._node_id)
         result = self._io_state.get_by_prefix(schema, self._parent_id)
@@ -146,7 +161,7 @@ class CommitState(State):
 
     def get_outputs(self, node_id) -> Optional[Any]:
         if self._io_state is None:
-            return
+            return None
         return self._io_state.get_by_prefix(node_id, self._parent_id)
 
     def get_inputs_by_transformer(self, transformer: Callable) -> dict:
@@ -166,39 +181,45 @@ class CommitState(State):
         self._io_state.commit()
         self._comp_state.commit()
         self._global_state.commit()
+        self._workflow_state.commit()
 
     def rollback(self) -> None:
         self._comp_state.rollback(self._node_id)
         self._io_state.rollback(self._node_id)
         self._global_state.rollback(self._node_id)
+        self._workflow_state.rollback(self._node_id)
 
     def get_state(self) -> dict:
         return {
             IO_STATE_KEY: self._io_state.get_state(),
             GLOBAL_STATE_KEY: self._global_state.get_state(),
             COMP_STATE_KEY: self._comp_state.get_state(),
+            WORKFLOW_STATE_KEY: self._workflow_state.get_state()
         }
 
     def set_state(self, state: dict) -> None:
         self._io_state.set_state(state.get(IO_STATE_KEY))
         self._global_state.set_state(state.get(GLOBAL_STATE_KEY))
         self._comp_state.set_state(state.get(COMP_STATE_KEY))
+        self._workflow_state.set_state(state.get(WORKFLOW_STATE_KEY))
 
     def get_updates(self) -> dict:
         return {
             IO_STATE_UPDATES_KEY: self._io_state.get_updates(),
             GLOBAL_STATE_UPDATES_KEY: self._global_state.get_updates(),
             COMP_STATE_UPDATES_KEY: self._comp_state.get_updates(),
+            WORKFLOW_STATE_UPDATES_KEY: self._workflow_state.get_updates()
         }
 
     def set_updates(self, updates: dict) -> None:
         self._io_state.set_updates(updates.get(IO_STATE_UPDATES_KEY))
         self._global_state.set_updates(updates.get(GLOBAL_STATE_UPDATES_KEY))
         self._comp_state.set_updates(updates.get(COMP_STATE_UPDATES_KEY))
+        self._workflow_state.set_updates(updates.get(WORKFLOW_STATE_UPDATES_KEY))
 
     def create_node_state(self, node_id: str, parent_id: str) -> Self:
         return CommitState(io_state=self._io_state, global_state=self._global_state,
-                           comp_state=self._comp_state,
+                           comp_state=self._comp_state, workflow_state=self._workflow_state,
                            trace_state=self._trace_state, node_id=node_id, parent_id=parent_id)
 
 
@@ -285,5 +306,6 @@ class InMemoryState(CommitState):
     def __init__(self, global_state: CommitStateLike = None):
         super().__init__(io_state=InMemoryCommitState(),
                          global_state=global_state if global_state is not None else InMemoryCommitState(),
+                         workflow_state=InMemoryCommitState(),
                          trace_state=dict(),
                          comp_state=InMemoryCommitState())

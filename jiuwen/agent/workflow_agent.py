8af29bee27827cda1617661f35249b5c2317b632
lidxCode <7810391+lidxCode@user.noreply.gitee.com>
2025-09-28 14:48:11 +0800

    feat:workflow agent adapt Config
    workflow agent adapt Config

--- jiuwen/agent/workflow_agent.py
+++ jiuwen/agent/workflow_agent.py
@@ -9,6 +9,7 @@ from jiuwen.core.context_engine.engine import ContextEngine
 from jiuwen.core.agent.controller.workflow_controller import WorkflowController
 from jiuwen.core.agent.agent import Agent
 from jiuwen.core.agent.handler.base import AgentHandlerImpl
+from jiuwen.core.runtime.config import Config
 from jiuwen.core.runtime.runtime import Runtime
 from jiuwen.core.stream.writer import TraceSchema
 from jiuwen.core.workflow.base import Workflow
@@ -34,25 +35,27 @@ def create_workflow_agent(agent_config: WorkflowAgentConfig,
 
 class WorkflowAgent(Agent):
     def __init__(self, agent_config: WorkflowAgentConfig):
-        super().__init__(agent_config)
+        self._config = Config()
+        self._config.set_agent_config(agent_config=agent_config)
+        super().__init__(self._config)
         self.context_engine = self._create_context_engine()
 
     def _init_controller(self):
         """初始化Controller - 延迟到invoke/stream时进行"""
-        if self._config.controller_type != ControllerType.WorkflowController:
+        if self._config.get_agent_config().controller_type != ControllerType.WorkflowController:
             raise NotImplementedError("")
         return None
 
     def _init_agent_handler(self):
-        return AgentHandlerImpl(self._config)
+        return AgentHandlerImpl(self._config.get_agent_config())
 
     def _create_context_engine(self) -> ContextEngine:
         """创建ContextEngine实例"""
         context_config = ContextEngineConfig(
-            conversation_history_length=self._config.constrain.reserved_max_chat_rounds * 2
+            conversation_history_length=self._config.get_agent_config().constrain.reserved_max_chat_rounds * 2
         )
         return ContextEngine(
-            agent_id=self._config.id,
+            agent_id=self._config.get_agent_config().id,
             config=context_config,
             model=None  # 可以根据需要传入模型
         )
@@ -60,7 +63,7 @@ class WorkflowAgent(Agent):
     def _create_controller(self, context_engine: ContextEngine, runtime: Runtime) -> WorkflowController:
         """创建WorkflowController实例"""
         controller = WorkflowController(
-            self._config,
+            self._config.get_agent_config(),
             context_engine,
             runtime
         )

fae4f53d3bd6cdbc05c2acb2717b2166defd6219
zhouguangliang <zhouguangliang5@huawei.com>
2025-11-18 11:33:22 +0800

    fix: 修正Reflector bug
    修正Reflector bug
    
    #ID4VZ0

--- openjiuwen/agent_builder/nl_to_agent/agent_builder/workflow_builder/dl_reflector/base.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved
-
-class Reflector:
-    def __init__(self):
-        pass
--- openjiuwen/agent_builder/nl_to_agent/agent_builder/workflow_builder/dl_reflector/dl_reflector.py
+++ openjiuwen/agent_builder/nl_to_agent/agent_builder/workflow_builder/dl_reflector/dl_reflector.py
@@ -14,13 +14,16 @@ def extract_placeholder_content(input_str: str) -> tuple[bool, list[str]]:
 
 class Reflector:
     def __init__(self):
-        self.available_node_types = ['Start', 'End', 'Output', 'LLM', 'Questioner',
-                                     'Plugin', 'Code', 'Branch', 'IntentDetection']
-        self.available_node_outputs = set()  # 已验证节点的输出变量，可以作为后续节点的输入引用
+        self.available_node_types = {'Start', 'End', 'Output', 'LLM', 'Questioner',
+                                     'Plugin', 'Code', 'Branch', 'IntentDetection'}
+        self.available_variable_types = {'String', 'Integer', 'Number', 'Boolean', 'Object',
+                               'Array<String>', 'Array<Integer>', 'Array<Number>', 'Array<Boolean>', 'Array<Object>'}
+        self.available_condition_operators = {"eq", "not_eq", "contain", "not_contain", "longer_than",
+                                "longer_than_or_eq", "short_than", "short_than_or_eq", "is_empty", "is_not_empty"}
+        self.available_node_outputs = set()
         self.node_ids = []
         self.node_ids_of_next = set()
-        self.variable_types = ['String', 'Integer', 'Number', 'Boolean', 'Object',
-                               'Array<String>', 'Array<Integer>', 'Array<Number>', 'Array<Boolean>', 'Array<Object>']
+
         self.errors = []
         self.check_functions = {
             'Start': self._check_start_node,
@@ -54,7 +57,7 @@ class Reflector:
                 self.errors.append(f"节点ID错误: {node_id} 不存在")
 
     def reset(self):
-        self.available_node_outputs = set()  # 已验证节点的输出变量，可以作为后续节点的输入引用
+        self.available_node_outputs = set()
         self.node_ids = []
         self.node_ids_of_next = set()
         self.errors = []
@@ -91,80 +94,44 @@ class Reflector:
 
     def _check_end_node(self, node_content: dict):
         self._check_inputs_list(node_content)
-        configs_format_has_error = self._check_configs_format(node_content)
-        if configs_format_has_error:
-            return
-        configs_keys = node_content['parameters']['configs'].keys()
-        if 'template' not in configs_keys:
-            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'configs'字典中缺失'template'属性")
+        self._check_configs(node_content, keys=['template'])
 
     def _check_output_node(self, node_content: dict):
-        self._check_next_missing(node_content)
         self._check_inputs_list(node_content)
-        configs_format_has_error = self._check_configs_format(node_content)
-        if configs_format_has_error:
-            return
-        configs_keys = node_content['parameters']['configs'].keys()
-        if 'template' not in configs_keys:
-            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'configs'字典中缺失'template'属性")
+        self._check_configs(node_content, keys=['template'])
+        self._check_next_missing(node_content)
 
     def _check_llm_node(self, node_content: dict):
-        self._check_next_missing(node_content)
         self._check_inputs_list(node_content)
         self._check_outputs_list(node_content)
-        configs_format_has_error = self._check_configs_format(node_content)
-        if configs_format_has_error:
-            return
-        configs_keys = node_content['parameters']['configs'].keys()
-        for key in ['system_prompt', 'user_prompt']:
-            if key not in configs_keys:
-                self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'configs'字典中缺失'{key}'属性")
+        self._check_configs(node_content, keys=['system_prompt', 'user_prompt'])
+        self._check_next_missing(node_content)
 
     def _check_questioner_node(self, node_content: dict):
-        self._check_next_missing(node_content)
         self._check_inputs_list(node_content)
         self._check_outputs_list(node_content)
-        configs_format_has_error = self._check_configs_format(node_content)
-        if configs_format_has_error:
-            return
-        configs_keys = node_content['parameters']['configs'].keys()
-        if 'prompt' not in configs_keys:
-            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'configs'字典中缺失'prompt'属性")
+        self._check_configs(node_content, keys=['prompt'])
+        self._check_next_missing(node_content)
 
     def _check_plugin_node(self, node_content: dict):
         """
         TODO: need check with the original plugin info.
         """
-        self._check_next_missing(node_content)
         self._check_inputs_list(node_content)
         self._check_outputs_list(node_content)
-        configs_format_has_error = self._check_configs_format(node_content)
-        if configs_format_has_error:
-            return
-        configs_keys = node_content['parameters']['configs'].keys()
-        if 'tool_id' not in configs_keys:
-            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'configs'字典中缺失'tool_id'属性")
-        
+        self._check_configs(node_content, keys=['tool_id'])
+        self._check_next_missing(node_content)
+
     def _check_code_node(self, node_content: dict):
         self._check_inputs_list(node_content)
         self._check_outputs_list(node_content)
-        configs_format_has_error = self._check_configs_format(node_content)
-        if configs_format_has_error:
-            return
-        configs_keys = node_content['parameters']['configs'].keys()
-        if 'code' not in configs_keys:
-            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'configs'字典中缺失'code'属性")
+        self._check_configs(node_content, keys=['code'])
         self._check_next_missing(node_content)
 
     def _check_intent_detection_node(self, node_content: dict):
         self._check_inputs_list(node_content)
+        self._check_configs(node_content, keys=['prompt'])
         self._check_intent_conditions_list(node_content)
-        configs_format_has_error = self._check_configs_format(node_content)
-        if configs_format_has_error:
-            return
-        configs_keys = node_content['parameters']['configs'].keys()
-        if 'prompt' not in configs_keys:
-            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'configs'字典中缺失'prompt'属性")
 
     def _check_intent_conditions_list(self, node_content: dict):
         if 'conditions' not in node_content['parameters']:
@@ -174,6 +141,48 @@ class Reflector:
             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'属性必须为列表类型")
             return
         node_id = node_content['id']
+        has_default_branch = False
+        for condition in node_content['parameters']['conditions']:
+            if not isinstance(condition, dict):
+                self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素必须为字典类型")
+                return
+            # check branch and description val
+            for key in ['branch', 'description']:
+                if key not in condition:
+                    self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素缺失'{key}'属性")
+            # check next
+            if 'next' not in condition:
+                self.errors.append(
+                    f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素缺失'next'属性")
+            else:
+                self.node_ids_of_next.add(condition["next"])
+            # check expression
+            if 'expression' not in condition:
+                self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素缺失'expression'属性")
+            else:
+                expression = condition['expression']
+                if not isinstance(expression, str):
+                    self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'属性必须为字符串类型")
+                    return
+                if expression == 'default':
+                    has_default_branch = True
+                else:
+                    left_val = "${" + node_id + ".rawOutput}"
+                    if left_val not in expression:
+                        self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'的表达式变量错误")
+                    if "contain" not in expression:
+                        self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'的表达式必须使用contain")
+        if not has_default_branch:
+            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中缺少default分支")
+
+    def _check_branch_node(self, node_content: dict):
+        if 'conditions' not in node_content['parameters']:
+            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中缺失'conditions'属性")
+            return
+        if not isinstance(node_content['parameters']['conditions'], list):
+            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'属性必须为列表类型")
+            return
+        has_default_branch = False
         for condition in node_content['parameters']['conditions']:
             if not isinstance(condition, dict):
                 self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素必须为字典类型")
@@ -183,54 +192,26 @@ class Reflector:
                 if key not in condition:
                     self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素缺失'{key}'属性")
             # check expression
-            if 'expression' not in condition:
-                self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素缺失'expression'属性")
+            if condition.get('expression') == 'default':
+                has_default_branch = True
             else:
-                expression = condition['expression']
-                if not isinstance(expression, str):
-                    self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'属性必须为字符串类型")
-                    return
-                left_val = "${" + node_id + ".rawOutput}"
-                if left_val not in expression:
-                    self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'的表达式变量错误")
-                if "contain" not in expression:
-                    self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'的表达式必须使用contain")
+                self._check_branch_expression(condition, node_content)
             # check next
             if 'next' not in condition:
                 self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素缺失'next'属性")
             else:
                 self.node_ids_of_next.add(condition["next"])
+        if not has_default_branch:
+            self.errors.append(
+                f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中缺少default分支")
 
-    def _check_branch_node(self, node_content: dict):
-        if 'conditions' not in node_content['parameters']:
-            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中缺失'conditions'属性")
-            return
-        if not isinstance(node_content['parameters']['conditions'], list):
-            self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'属性必须为列表类型")
-            return
-
-        for condition in node_content['parameters']['conditions']:
-            if not isinstance(condition, dict):
-                self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素必须为字典类型")
-                return
-            # check branch and description val
-            for key in ['branch', 'description']:
-                if key not in condition:
-                    self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素缺失'{key}'属性")
-            # check expression            
-            self._check_branch_expression(condition, node_content)
-            # check next
-            if 'next' not in condition:
-                self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素缺失'next'属性")
-            else:
-                self.node_ids_of_next.add(condition["next"])
-        
     def _check_branch_expression(self, condition_branch: dict, node_content: dict):
         if 'expression' in condition_branch:
             expression = condition_branch['expression']
             if not isinstance(expression, str):
                 self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'属性必须为字符串类型")
             else:
+                self._check_branch_operator(expression, node_content)
                 _, placeholder_content = extract_placeholder_content(expression)
                 for content in placeholder_content:
                     if content not in self.available_node_outputs:
@@ -241,12 +222,19 @@ class Reflector:
                 self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'属性必须为列表类型")
             else:
                 for expression in expressions:
+                    self._check_branch_operator(expression, node_content)
                     _, placeholder_content = extract_placeholder_content(expression)
                     for content in placeholder_content:
                         if content not in self.available_node_outputs:
                             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'的表达式中引用了不存在的变量")
         else:
             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素缺失'expression'或'expressions'属性")
+
+    def _check_branch_operator(self, expression: str, node_content: dict):
+        expression_list = expression.strip().split(" ")
+        if expression_list[1] not in self.available_condition_operators:
+            self.errors.append(
+                f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'conditions'列表中的元素的'expression'的表达式使用了不支持的关系运算符")
 
     def _check_inputs_list(self, node_content: dict, check_type: bool = False):
         """
@@ -263,7 +251,7 @@ class Reflector:
         if 'inputs' not in node_content['parameters']:
             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中缺失'inputs'属性")
             return
-        if isinstance(node_content['parameters']['inputs'], list):
+        if not isinstance(node_content['parameters']['inputs'], list):
             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'inputs'属性必须为列表类型")
             return
         input_names = set()
@@ -295,8 +283,8 @@ class Reflector:
                 if 'type' not in input_item:
                     self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'inputs'列表中的元素缺失'type'属性")
                 else:
-                    if input_item['type'] not in self.variable_types:
-                        self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'inputs'列表中的元素'type'属性值必须为{self.variable_types}中的一个")
+                    if input_item['type'] not in self.available_variable_types:
+                        self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'inputs'列表中的元素'type'属性值必须为{self.available_variable_types}中的一个")
 
     def _check_outputs_list(self, node_content: dict, check_type: bool = False):
         """
@@ -311,7 +299,7 @@ class Reflector:
         if 'outputs' not in node_content['parameters']:
             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中缺失'outputs'属性")
             return
-        if isinstance(node_content['parameters']['outputs'], list):
+        if not isinstance(node_content['parameters']['outputs'], list):
             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'outputs'属性必须为列表类型")
             return
         output_names = set()
@@ -334,20 +322,23 @@ class Reflector:
                 if 'type' not in output_item:
                     self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'outputs'列表中的元素缺失'type'属性")
                 else:
-                    if output_item['type'] not in self.variable_types:
-                        self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'outputs'列表中的元素'type'属性值必须为{self.variable_types}中的一个")
+                    if output_item['type'] not in self.available_variable_types:
+                        self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'outputs'列表中的元素'type'属性值必须为{self.available_variable_types}中的一个")
 
-    def _check_configs_format(self, node_content: dict) -> bool:
+    def _check_configs(self, node_content: dict, keys: list):
         if 'configs' not in node_content['parameters']:
             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中缺失'configs'属性")
-            return True
+            return
         if not isinstance(node_content['parameters']['configs'], dict):
             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'configs'属性必须为字典类型")
-            return True
-        return False
+            return
+        configs_keys = node_content['parameters']['configs'].keys()
+        for key in keys:
+            if key not in configs_keys:
+                self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 'parameters'中的'configs'字典中缺失'{key}'属性")
 
     def _check_next_missing(self, node_content: dict):
         if 'next' not in node_content:
             self.errors.append(f"{node_content['id']}节点, 类型为{node_content['type']}, 缺失'next'属性")
         else:
-            self.node_ids_of_next.add(node_content["next"])
+            self.node_ids_of_next.add(node_content['next'])

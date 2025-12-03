d20dbf29661d870bb69a3639cd61c853f87945e2
yangwenhan <yangwenhan@huawei.com>
2025-12-03 10:39:31 +0800

    fix: modify DefaultDbStore var name

--- openjiuwen/core/memory/store/impl/default_db_store.py
+++ openjiuwen/core/memory/store/impl/default_db_store.py
@@ -7,8 +7,8 @@ from openjiuwen.core.memory.store.base_db_store import BaseDbStore
 
 
 class DefaultDbStore(BaseDbStore):
-    def __init__(self, async_engine: AsyncEngine):
-        self.async_engine = async_engine
+    def __init__(self, async_conn: AsyncEngine):
+        self.async_conn = async_conn
 
     def get_async_engine(self) -> AsyncEngine:
-        return self.async_engine
+        return self.async_conn

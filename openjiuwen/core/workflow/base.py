3c47c15202349886d58f335da3b47d556723ca8f
laihongsen <laihongsen@huawei.com>
2025-12-04 15:18:30 +0800

    test(workflow): 增加对未注册边节点的检测测试
    - 添加测试用例验证_auto_complete_abilities能检测到未注册组件
    - 增加invoke方法中对未注册源节点和目标节点的验证测试
    - 确保错误信息包含有用的调试信息，如未注册节点名和已注册组件列表
    - 验证合法连接仍能正常执行工作流

--- openjiuwen/core/workflow/base.py
+++ openjiuwen/core/workflow/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright (c) Huawei Technologies Co. Ltd. 2025-2025. All rights reserved.
+# Copyright c) Huawei Technologies Co. Ltd. 2025-2025. All rights reserved.
 import asyncio
 import inspect
 import os

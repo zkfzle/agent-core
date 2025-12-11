5fe44b6048dc15ad82f9d876657cfce10da7f0fb
lairuofei <lairuofei@noreply.gitcode.com>
2025-12-11 14:36:13 +0800

    refactor: remove integrations module and llama-index dependency

--- openjiuwen/integrations/retriever/retrieval/embed_models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
-
-from openjiuwen.integrations.retriever.retrieval.embed_models.api import APIEmbedModel
-from openjiuwen.integrations.retriever.retrieval.embed_models.base import EmbedModel
-from openjiuwen.integrations.retriever.retrieval.embed_models.ollama import OllamaEmbedModel
--- openjiuwen/integrations/retriever/retrieval/search/agents/prompts/__init__.py
+++
@@ -1,1 +0,0 @@
-# Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.

70b968ccac9696b52a6c5f13307aabd30e9088ae
alan_cheng <chengshuo5@huawei.com>
2025-12-20 22:46:52 +0800

    refactor: add workflow system tests
    Refs: #11

--- jiuwen/core/context_engine/config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-
-from pydantic import BaseModel, Field
-
-
-class ContextEngineConfig(BaseModel):
-    conversation_history_length: int = Field(default=20, ge=0)

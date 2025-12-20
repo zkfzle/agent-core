fe18ff640f191b26d90888a3de51a2dcde9e47ed
alan_cheng <chengshuo5@huawei.com>
2025-12-20 22:46:52 +0800

    refactor: remove invalid folders
    Refs: #11

--- jiuwen/core/common/constants/component.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-
-START_COMPONENT = "start"
-END_COMPONENT = "end"
-BRANCH_COMPONENT = "branch"
-LOOP_COMPONENT = "loop"
-SUB_WORKFLOW_COMPONENT = "sub_workflow"
--- jiuwen/core/graph/graph_state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-import operator
-from typing import TypedDict, Annotated
-
-
-class GraphState(TypedDict):
-    source_node_id: Annotated[list, operator.add]

d1b6b3179fcf8c01ae289a6f15c0655fb66aa1f2
Ziliang_Zhou <zhouziliang4@huawei.com>
2025-12-27 16:46:56 +0800

    feat: message summary extract

--- openjiuwen/core/memory/generation/memory_analyzer.py
+++ openjiuwen/core/memory/generation/memory_analyzer.py
@@ -11,7 +11,10 @@ from openjiuwen.core.utils.llm.base import BaseModelClient
 from openjiuwen.core.utils.llm.messages import BaseMessage
 from openjiuwen.core.utils.llm.output_parser.json_output_parser import JsonOutputParser
 from openjiuwen.core.memory.config.config import MemoryConfig
-from openjiuwen.core.memory.prompt.memory_analyzer import MEMORY_ANALYZER_PROMPT, VARIABLES_DESCRIPTION_TEMPLATE_PROMPT
+from openjiuwen.core.memory.prompt.memory_analyzer import (MEMORY_ANALYZER_PROMPT,
+                                                           VARIABLES_DESCRIPTION_TEMPLATE_PROMPT,
+                                                           SUMMARY_TEMPLATE_PROMPT)
+
 
 
 class VariableResult(BaseModel):
@@ -22,6 +25,7 @@ class VariableResult(BaseModel):
 class MemoryAnalyzerResult(BaseModel):
     categories: List[str] = Field(default=[])
     variables: List[VariableResult] = Field(default=[])
+    summary: str = Field(default="")
 
 
 class MemoryAnalyzer:
@@ -34,6 +38,8 @@ class MemoryAnalyzer:
             history_messages: List[BaseMessage],
             base_chat_model: Tuple[str, BaseModelClient],
             memory_config: MemoryConfig,
+            summary_max_length_threshold: int = 50,
+            summary_max_token: int = 30,
             retries: int = 3
     ) -> MemoryAnalyzerResult | None:
         if len(messages) == 0:
@@ -43,6 +49,8 @@ class MemoryAnalyzer:
             messages=messages,
             history_messages=history_messages,
             memory_config=memory_config,
+            summary_max_length_threshold=summary_max_length_threshold,
+            summary_max_token=summary_max_token
         )
 
         model_name, model_client = base_chat_model
@@ -64,11 +72,25 @@ class MemoryAnalyzer:
     def _build_model_input(
             messages: List[BaseMessage],
             history_messages: List[BaseMessage],
-            memory_config: MemoryConfig
+            memory_config: MemoryConfig,
+            summary_max_length_threshold: int,
+            summary_max_token: int
     ) -> List:
         variables_description, variables_output_format = MemoryAnalyzer._build_variable_prompt(memory_config)
+        total_message_length = len(messages[0].content)
+        if total_message_length >= summary_max_length_threshold:
+            summary_description, summary_output_format = \
+                MemoryAnalyzer._build_summary_prompt(
+                    max_message_token=summary_max_token,
+                    memory_config=memory_config
+                )
+        else:
+            summary_description, summary_output_format = "", ""
+
         sys_prompt = MEMORY_ANALYZER_PROMPT.replace("VARIABLES_DESCRIPTION_TEMPLATE", variables_description)
         sys_prompt = sys_prompt.replace("VARIABLES_OUTPUT_TEMPLATE", variables_output_format)
+        sys_prompt = sys_prompt.replace("SUMMARY_TEMPLATE", summary_description)
+        sys_prompt = sys_prompt.replace("SUMMARY_OUTPUT_TEMPLATE", summary_output_format)
         model_input = [{
             "role": "system",
             "content": sys_prompt
@@ -115,3 +137,16 @@ class MemoryAnalyzer:
         return VARIABLES_DESCRIPTION_TEMPLATE_PROMPT.replace("VARIABLES_DEFINE_TEMPLATE",
                                                              variables_description_json), \
             ",\n\"variables\":" + variables_output_format_json
+
+    @staticmethod
+    def _build_summary_prompt(
+            max_message_token: int,
+            memory_config: MemoryConfig
+    ) -> Tuple[str, str]:
+        if len(memory_config.mem_variables) == 0:
+            SUMMARY_TEMPLATE_PROMPT.format(step_num=2)
+        else:
+            SUMMARY_TEMPLATE_PROMPT.format(step_num=3)
+        SUMMARY_TEMPLATE_PROMPT.format(max_message_token=max_message_token)
+        summary_output_format = ',\n\"summary\": ""'
+        return SUMMARY_TEMPLATE_PROMPT, summary_output_format

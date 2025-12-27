56decd9b2b115da1b0732c9df383a076dba5999d
weixin_42161333 <wangxiaolong100@huawei.com>
2025-12-27 09:41:01 +0800

    feat(common): base card

---
+++ openjiuwen/core/common/schema/card.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+
+from pydantic import BaseModel
+
+
+class BaseCard(BaseModel):
+    """数字名片基类
+
+    Attributes:
+        id: 唯一标识符
+        name: 名称，也是在某个 namespace 中的唯一标识符
+        description: 功能、适用场景等描述信息
+    """
+    id: str
+    name: str
+    description: str

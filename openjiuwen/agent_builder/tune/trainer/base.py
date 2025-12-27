4efae7e57b9e5e008a50fcf4dd2e21d40a78f68d
l_x_d <l_x_d@noreply.gitcode.com>
2025-12-27 14:20:32 +0800

    feat: 提示词优化展示评估详情数据

--- openjiuwen/agent_builder/tune/trainer/base.py
+++ openjiuwen/agent_builder/tune/trainer/base.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-from typing import Generator
+from typing import Generator, List
 from pydantic import BaseModel, Field
 
 from openjiuwen.core.agent.agent import Agent
-from openjiuwen.agent_builder.tune.base import TuneConstant
+from openjiuwen.agent_builder.tune.base import TuneConstant, EvaluatedCase
 
 
 class Progress(BaseModel):
-    val_baseline_score: float = Field(default=0.0, ge=0.0, le=1.0)
     current_epoch: int = Field(default=0, ge=0)
     max_epoch: int = Field(default=TuneConstant.DEFAULT_ITERATION_NUM, ge=0)
     current_batch_iter: int = Field(default=0, ge=0)
     max_batch_iter: int = Field(default=1, ge=0)
     best_score: float = Field(default=0.0, ge=0.0, le=1.0)
     best_batch_score: float = Field(default=0.0, ge=0.0, le=1.0)
+    current_epoch_score: float = Field(default=0.0, ge=0.0, le=1.0)
 
     def run_epoch(self) -> Generator:
         for epoch in range(1, self.max_epoch + 1):
@@ -30,14 +30,14 @@ class Progress(BaseModel):
 
 
 class Callbacks:
-    def on_train_begin(self, agent: Agent, progress: Progress) -> None:
+    def on_train_begin(self, agent: Agent, progress: Progress, eval_info: List[EvaluatedCase]) -> None:
         pass
 
-    def on_train_end(self, agent: Agent, progress: Progress) -> None:
+    def on_train_end(self, agent: Agent, progress: Progress, eval_info: List[EvaluatedCase]) -> None:
         pass
 
     def on_train_epoch_begin(self, agent: Agent, progress: Progress) -> None:
         pass
 
-    def on_train_epoch_end(self, agent: Agent, progress: Progress) -> None:
+    def on_train_epoch_end(self, agent: Agent, progress: Progress, eval_info: List[EvaluatedCase]) -> None:
         pass

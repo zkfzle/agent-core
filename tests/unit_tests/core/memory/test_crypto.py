91ce3248a032b9893ee226eb253b59a805f0cf52
zhangmengyin <zhangmengyin2@huawei.com>
2025-12-29 12:04:57 +0800

    test(memory): Add test case for chroma semantic store
    1.Add test_chroma_semantic_store.py
    
    Refs: #38

--- tests/unit_tests/core/memory/test_crypto.py
+++ tests/unit_tests/core/memory/test_crypto.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-import unittest
+import pytest
 from openjiuwen.core.memory.common.crypto import encrypt, decrypt
 
 
-class TestCrypto(unittest.TestCase):
-    def test_encrypt(self):
+class TestCrypto:
+    @staticmethod
+    def test_encrypt():
         test_key = b'1234567890abcdef1234567890123456'
         test_data = "hello, 我叫张三, xixi"
         encrypt_data, nonce, tag = encrypt(test_key, test_data)
         decrypt_data = decrypt(test_key, encrypt_data, nonce, tag)
-        self.assertEqual(decrypt_data, test_data)
+        assert decrypt_data == test_data
 
-    def test_key_error(self):
+    @staticmethod
+    def test_key_error():
         test_key = b'1234567890abcedfgg'
         test_data = "你好, 我叫李四"
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             encrypt(test_key, test_data)

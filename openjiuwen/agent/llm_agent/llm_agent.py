34e71ac61ceef17dacb578b475e341c4d7bb3aa6
Stanley_Ren <Stanley_Ren@noreply.gitcode.com>
2025-12-30 23:04:56 +0800

    update: 更新文件 llm_agent.py keep copyright format consist
    Signed-off-by: Stanley_Ren <Stanley_Ren@noreply.gitcode.com>

--- openjiuwen/agent/llm_agent/llm_agent.py
+++ openjiuwen/agent/llm_agent/llm_agent.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved
 """LLMAgent - ReAct style Agent based on ControllerAgent"""
 import asyncio
 import datetime

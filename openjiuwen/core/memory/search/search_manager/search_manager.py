18760295c07e1b9ea9d61765c03d210ac5c3cfe0
miaoran_li <miaoran_li@noreply.gitcode.com>
2026-01-05 14:36:58 +0800

    fix(memory): add log for query rewrite

--- openjiuwen/core/memory/search/search_manager/search_manager.py
+++ openjiuwen/core/memory/search/search_manager/search_manager.py
@@ -66,10 +66,13 @@ class SearchManager:
             decomposed_queries = [query]
         retrieval_results = []
 
+        log_retrieval_result = []
         for subquery in decomposed_queries:
             subresults = await self.search(user_id=user_id, group_id=group_id, query=subquery, top_k=top_k,
                                            threshold=threshold, search_type=search_type, **kwargs)
+            log_retrieval_result.append(f"Search results for subquery <{subquery}>: {subresults}")        
             retrieval_results.extend(subresults)
+        logger.info(" | ".join(log_retrieval_result))
 
         dedup_results = {}
         for item in retrieval_results:
@@ -79,6 +82,7 @@ class SearchManager:
 
         results = list(dedup_results.values())
         results.sort(key=lambda x: x["score"], reverse=True)
+        logger.info(f"Final Search Results: {results}")
         return [item for item in results if item["score"] >= threshold][:top_k]
 
     async def decompose_query(self, base_chat_model: Tuple[str, BaseModelClient], query: str) -> list[str] | None:

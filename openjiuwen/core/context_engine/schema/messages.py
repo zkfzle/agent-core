0b3e2895ea5a92c89bf26aed82a23f7142c55e9b
zhangshuai117 <zhangshuai117@huawei.com>
2026-01-10 10:56:06 +0800

    feat(context): add processor framwork for context engine

---
+++ openjiuwen/core/context_engine/schema/messages.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+
+from abc import abstractmethod
+import uuid
+from typing import List
+from pydantic import Field
+
+from openjiuwen.core.foundation.llm1 import BaseMessage
+
+
+class OffloadMessage(BaseMessage):
+    offload_id: str = Field(default_factory=lambda x: uuid.uuid4().hex)
+
+    @abstractmethod
+    async def reload(self) -> List[BaseMessage]:
+        pass
+
+    @abstractmethod
+    async def offload(self, messages: List[BaseMessage]):
+        pass
+
+
+class MemoryOffloadMessage(OffloadMessage):
+    original_messages: List[BaseMessage] = Field(default=None)
+
+    async def reload(self) -> List[BaseMessage]:
+        return self.original_messages
+
+    async def offload(self, messages: List[BaseMessage]):
+        self.original_messages = messages

9db046663aad930fcf9f17ce7566200a6f7a09a4
zhangmengyin <zhangmengyin2@huawei.com>
2026-01-12 16:22:19 +0800

    feat(memory): new chroma interface based on VectorStore
    1.Add ChromaVectorStore based on VectorStore
    2.Add test cases
    3.Delete ChromaSemanticStore and test_chroma_semantic_store.py
    
    Refs: #89

--- openjiuwen/core/memory/__init__.py
+++ openjiuwen/core/memory/__init__.py
@@ -2,6 +2,7 @@ from openjiuwen.core.memory.config import MemoryEngineConfig, MemoryScopeConfig
 from openjiuwen.core.memory.store import BaseKVStore, BaseSemanticStore, BaseDbStore
 from openjiuwen.core.memory.long_term_memory import LongTermMemory
 from openjiuwen.core.memory.store.impl.milvus_vector_store import MilvusVectorStore
+from openjiuwen.core.memory.store.impl.chroma_vector_store import ChromaVectorStore
 
 __all__ = [
     'MemoryEngineConfig',
@@ -10,5 +11,6 @@ __all__ = [
     'BaseSemanticStore',
     'BaseDbStore',
     'MilvusVectorStore',
+    'ChromaVectorStore',
     'LongTermMemory'
 ]

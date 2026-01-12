e038fd91a0373de34c582ddee5ae08708632fd53
hzx <huzhixing5@huawei.com>
2026-01-12 15:55:54 +0800

    feat(memory): Supports memory base capabilities
    Refs: #89

--- openjiuwen/core/memory/__init__.py
+++ openjiuwen/core/memory/__init__.py
@@ -1,13 +1,13 @@
-from openjiuwen.core.memory.config import MemoryEngineConfig, MemoryScopeConfig
-from openjiuwen.core.memory.store import BaseKVStore, BaseSemanticStore, BaseDbStore
+from openjiuwen.core.memory.config import MemoryEngineConfig, MemoryScopeConfig, MemoryAgentConfig
+from openjiuwen.core.memory.store import BaseKVStore, BaseDbStore
 from openjiuwen.core.memory.long_term_memory import LongTermMemory
 from openjiuwen.core.memory.store.impl.milvus_vector_store import MilvusVectorStore
 
 __all__ = [
     'MemoryEngineConfig',
     'MemoryScopeConfig',
+    'MemoryAgentConfig',
     'BaseKVStore',
-    'BaseSemanticStore',
     'BaseDbStore',
     'MilvusVectorStore',
     'LongTermMemory'

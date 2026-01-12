bdcda1dc65fcef3a471ffcbf6202e261c8d60d0a
hzx <huzhixing5@huawei.com>
2026-01-12 15:55:54 +0800

    feat(memory): Supports memory base capabilities
    Refs: #89

--- openjiuwen/core/memory/store/__init__.py
+++ openjiuwen/core/memory/store/__init__.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 # Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+
 from openjiuwen.core.memory.store.base_db_store import BaseDbStore
 from openjiuwen.core.memory.store.base_kv_store import BaseKVStore
-from openjiuwen.core.memory.store.base_semantic_store import BaseSemanticStore
+from openjiuwen.core.memory.store.semantic_store import SemanticStore
 
-__all__ = ["BaseDbStore", "BaseKVStore", "BaseSemanticStore"]
+__all__ = ["BaseDbStore", "BaseKVStore", "SemanticStore"]

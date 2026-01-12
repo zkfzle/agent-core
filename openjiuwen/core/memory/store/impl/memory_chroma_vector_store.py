d1f82cc594827173fc22232ee66a39d5e518de29
undercrater <zhaoyulu1@huawei.com>
2026-01-12 10:45:18 +0800

    feat(memory): memory engine default kv store implementation
    Refs: #87

--- openjiuwen/core/memory/store/impl/memory_chroma_vector_store.py
+++ openjiuwen/core/memory/store/impl/memory_chroma_vector_store.py
@@ -20,6 +20,10 @@ class MemoryChromaVectorStore(VectorStore):
 
         self.collection_cache = {}  # Cache for collections
 
+    def create_client(database_name: str, path_or_uri: str, token: str = "", **kwargs) -> Any:
+        logger.error("create_client not implemented")
+        pass
+
     async def get_collection(self, table_name: str):
         """Get or create collection by table name"""
         if table_name in self.collection_cache:

44299c7e2a84eb1fa62722b80e882e8672dc91f8
wangxiaolong100 <wangxiaolong100@huawei.com>
2026-01-14 10:27:25 +0800

    Refactor(tool): remove ToolCall from tool
    Refs: #90

--- openjiuwen/core/foundation/tool/schema.py
+++ openjiuwen/core/foundation/tool/schema.py
@@ -1,9 +1,7 @@
 # coding: utf-8
 # Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-from typing import Dict, Any, Optional, Type, Union
-from jsonschema import validate
+from typing import Dict, Any, Type, Union
 from pydantic import BaseModel, Field
-from openjiuwen.core.common.schema.card import BaseCard
 
 
 
@@ -12,11 +10,3 @@ class ToolInfo(BaseModel):
     name: str = Field(default="")
     description: str = Field(default="")
     parameters: Union[Dict[str, Any], Type[BaseModel]] = Field(default_factory=dict)
-
-
-class ToolCall(BaseModel):
-    id: Optional[str]
-    type: str
-    name: str
-    arguments: str
-    index: Optional[int] = None

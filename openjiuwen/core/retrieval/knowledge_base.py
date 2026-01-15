b8bdefb1c9a6f626d8e13b888641cc24f9ff6097
SushiNinja <jin.huang2@h-partners.com>
2026-01-15 22:27:41 +0000

    fix: typo in error code

--- openjiuwen/core/retrieval/knowledge_base.py
+++ openjiuwen/core/retrieval/knowledge_base.py
@@ -51,7 +51,7 @@ class KnowledgeBase(ABC):
             index_manager_db = getattr(index_manager, "database_name", None)
             if vector_store_db != index_manager_db:
                 raise JiuWenBaseException(
-                    error_code=StatusCode.RETRIEVAL_KB_DATABASE_CONFIG_INVALID,
+                    error_code=StatusCode.RETRIEVAL_KB_DATABASE_CONFIG_INVALID.code,
                     message="Database name mismatch between vector_store and index_manager:\n"
                     f'- Vector Store ({type(vector_store).__name__}) is using "{vector_store_db}".\n'
                     f'- Index manager ({type(index_manager).__name__}) is using "{index_manager_db}"',

5e0b73002a9754ac478544e3d9bcbc993456479d
SushiNinja <jin.huang2@h-partners.com>
2026-01-16 03:23:35 +0800

    fix: fix typo in error message

--- openjiuwen/core/retrieval/embedding/ollama_embedding.py
+++ openjiuwen/core/retrieval/embedding/ollama_embedding.py
@@ -136,7 +136,7 @@ class OllamaEmbedding(Embedding):
         if not isinstance(callback_cls, type) or not issubclass(callback_cls, BaseCallback):
             raise JiuWenBaseException(
                 StatusCode.RETRIEVAL_EMBEDDING_INVALID_CALLBACK_CLASS.code,
-                "APIEmbedding.embed_documents received invalid callback_cls argument",
+                "OllamaEmbedding.embed_documents received invalid callback_cls argument",
             )
 
         # Filter out empty texts

2a1e6cd4559ff873e06e00a713e14454751dff0f
SushiNinja <jin.huang2@h-partners.com>
2026-01-16 11:03:47 +0000

    style: follow naming convention of StatusCode Enum

--- openjiuwen/core/retrieval/indexing/indexer/chroma_indexer.py
+++ openjiuwen/core/retrieval/indexing/indexer/chroma_indexer.py
@@ -70,7 +70,7 @@ class ChromaIndexer(Indexer):
                 self._distance_metric = "ip"
             case _:
                 raise JiuWenBaseException(
-                    error_code=StatusCode.RETRIEVAL_INDEXING_INVALID_DISTANCE_METRIC.code,
+                    error_code=StatusCode.RETRIEVAL_INDEXING_DISTANCE_METRIC_INVALID.code,
                     message=f'Invalid {distance_metric=} selected, must be one of ["cosine", "euclidean", "dot"]',
                 )
 

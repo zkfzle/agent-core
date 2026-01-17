462968b18789f3956c4f9780253f7a0cea55ba07
Ziliang_Zhou <zhouziliang4@huawei.com>
2026-01-17 11:05:14 +0800

    fix(memory): when pass scope_id in delete_mem_by_scope, delete all related scope_id collection in vector store
    Refs: #101
    
    Co-authored-by: Ziliang_Zhou<zhouziliang4@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !132 merge develop into develop
    
    fix(memory): when pass scope_id in delete_mem_by_scope, delete all related scope_id collection in vector store
    
    Created-by: Ziliang_Zhou
    Commit-by: Ziliang_Zhou
    Merged-by: openJiuwen-bot
    Description: <!--  Thanks for sending a pull request!  Here are some tips for you:
    
    1) If this is your first time, please read our contributor guidelines: https://gitcode.com/openJiuwen/community/blob/master/CONTRIBUTING.md
    
    2) If you want to contribute your code but don't know who will review and merge, please add label `openjiuwen-assistant` to the pull request, we will find and do it as soon as possible.
    -->
    
    **What type of PR is this?**
    <!-- 
    选择下面一种标签替换下方 `/kind <label>`，可选标签类型有：
    - /kind bug 
    - /kind task
    - /kind feature
    - /kind refactor
    - /kind clean_code
    如PR描述不符合规范，修改PR描述后需要/check-pr重新检查PR规范。
    -->
    /kind bug
    
    
    **Self-checklist**:（**请自检，在[ ]内打上x，我们将检视你的完成情况，否则会导致pr无法合入**）
    
    + - [ ] **设计**：PR对应的方案是否已经经过Maintainer评审，方案检视意见是否均已答复并完成方案修改
    + - [x] **测试**：PR中的代码是否已有UT/ST测试用例进行充分的覆盖，新增测试用例是否随本PR一并上库或已经上库
    + - [ ] **验证**：PR描述信息中是否已包含对该PR对应的Feature、Refactor、Bugfix的预期目标达成情况的详细验证结果描述
    + - [ ] **接口**：是否涉及对外接口变更，相应变更已得到接口评审组织的通过，API对应的注释信息已经刷新正确
    + - [ ] **文档**：是否涉及官网文档修改，如果涉及请及时提交资料到Doc仓
    
    <!-- **Special notes for your reviewers**: -->
    <!-- + - [ ] 是否导致无法前向兼容 -->
    <!-- + - [ ] 是否涉及依赖的三方库变更 -->
    
    
    See merge request: openJiuwen/agent-core!132

---
+++ openjiuwen/core/memory/manage/scope_user_mapping_manager.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+from typing import Any
+from openjiuwen.core.memory.store.sql_db_store import SqlDbStore
+
+
+class ScopeUserMappingManager:
+    def __init__(self,
+                 sql_db_store: SqlDbStore):
+        self.sql_db = sql_db_store
+        self.meta_table = "scope_user_mapping"
+
+    async def add(self, user_id: str, scope_id: str, **kwargs):
+        data = {
+            'user_id': user_id or '',
+            'scope_id': scope_id or '',
+        }
+        exists = await self.sql_db.exist(
+            table=self.meta_table,
+            conditions={
+                "user_id": data["user_id"],
+                "scope_id": data["scope_id"],
+            },
+        )
+        if exists:
+            return
+        await self.sql_db.write(self.meta_table, data)
+
+    async def delete_by_scope_id(self, scope_id: str) -> bool:
+        return await self.sql_db.delete(
+            table=self.meta_table,
+            conditions={"scope_id": scope_id}
+        )
+
+    async def get_by_scope_id(self, scope_id: str) -> list[dict[str, Any]] | None:
+        results = await self.sql_db.condition_get(
+            table=self.meta_table,
+            conditions={"scope_id": [scope_id]},
+            columns=None
+        )
+        return results if results else None
